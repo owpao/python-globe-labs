ffc923f17b19e65a48d62a084fc7cd407fa424b1
owpao <johnpaoloflores@gmail.com>
2019-05-07 22:37:30 -0700

    git init

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 def hello_world():
     return 'Hello World!'
 
-@app.route('/register')
+@app.route('/code/<code:code>')
 def register(code):
     print("--------code received--------")
     print(code)
